4dc38250926a896c88b38aaa5f93a036916e55ad
Diego Esteves <esteves@informatik.uni-leipzig.de>
2015-12-09 19:22:50 +0100

    polishing fox feature

--- defacto-core/src/main/java/org/aksw/defacto/nlp/ner/FOXNamedEntityRecognition.java
+++ defacto-core/src/main/java/org/aksw/defacto/nlp/ner/FOXNamedEntityRecognition.java
@@ -1,5 +1,7 @@
 package org.aksw.defacto.nlp.ner;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -8,10 +10,13 @@ import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.LoggerFactory;
 
 import java.nio.charset.Charset;
+import java.util.Collection;
 
 /**
  * Created by dnes on 08/12/15.
@@ -20,26 +25,76 @@ public class FOXNamedEntityRecognition {
 
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(StanfordNLPNamedEntityRecognition.class);
 
+    private static final String FOX_LIGHT_SPOTLIGHT_EN = "org.aksw.fox.tools.ner.en.SpotlightEN";
+    private static final String FOX_LIGHT_TAGME_EN = "org.aksw.fox.tools.ner.en.TagMeEN";
+    private static final String FOX_LIGHT_STANFORD_EN = "org.aksw.fox.tools.ner.en.StanfordEN";
+    private static final String FOX_LIGHT_OPENNLP_EN = "org.aksw.fox.tools.ner.en.OpenNLPEN";
+    private static final String FOX_LIGHT_NERDML_EN = "org.aksw.fox.tools.ner.en.NerdMLEN";
+    private static final String FOX_LIGHT_ILLINOISEXTENDED_EN = "org.aksw.fox.tools.ner.en.IllinoisExtendedEN";
+    private static final String FOX_LIGHT_BALIE_EN = "org.aksw.fox.tools.ner.en.BalieEN";
+
+    private static final String FOX_PERSON = "[\"scmsann:PERSON\",\"ann:Annotation\"]";
+    private static final String FOX_LOCATION = "[\"scmsann:LOCATION\",\"ann:Annotation\"]";
+
+    private static Multimap<String, String> multimap;
 
     public static void main(String[] args) {
 
-        String test = "This is a year 1872 this is not a year 345 this is also not a year 3323 but his 1293 2013";
-        FOXNamedEntityRecognition t  = new FOXNamedEntityRecognition();
-        System.out.println(t.getAnnotatedSenteces(test));
-    }
+        Collection<String> locations;
+        Collection<String> persons;
 
+        String test = "The philosopher and mathematician Leibniz was born in Leipzig";
+        FOXNamedEntityRecognition t  = new FOXNamedEntityRecognition();
+        Multimap<String, String> map;
+
+        map = t.getAnnotatedArray(test);
+
+        if (map.containsKey("LOCATION")){
+            locations = map.get("LOCATION");
+            persons = map.get("PERSON");
+        }
+
+    }
 
     public FOXNamedEntityRecognition(){
+        multimap = ArrayListMultimap.create();
+    }
+
+    public Multimap<String,String> getAnnotatedArray(String sentence){
+
+        try{
+            JSONObject myObject = new JSONObject(getAnnotatedSenteces(sentence));
+
+            JSONArray foxArray = myObject.getJSONArray("@graph");
+            for (int i = 0, size = foxArray.length(); i < size; i++) {
+                JSONObject oFOX = foxArray.getJSONObject(i);
+                String[] elementNames = JSONObject.getNames(oFOX);
+                for (String elementName : elementNames)
+                {
+                    String value = oFOX.getString(elementName);
+                    System.out.printf("name=%s, value=%s\n", elementName, value);
+
+                    if (elementName.equals("@type") == true && value.equals(FOX_LOCATION) == true){
+                        multimap.put("LOCATION",oFOX.getString("ann:body"));
+                    }else if (elementName.equals("@type") ==true && value.equals(FOX_PERSON) ==true){
+                        multimap.put("PERSON",oFOX.getString("ann:body"));
+                    }
+                }
+            }
+
+        }catch (JSONException e) {
+            LOG.error("Error on converting the FOX Object: " + e.toString());
+        }catch (Exception e){
+            LOG.error("Error: " + e.toString());
+        }
+
+        return multimap;
 
     }
 
-
-
-    public String getAnnotatedSenteces(String s){
-
+    private String getAnnotatedSenteces(String s){
 
         try{
-
 
             /*
             input : text or an url (e.g.: `G. W. Leibniz was born in Leipzig`, `http://en.wikipedia.org/wiki/Leipzig_University`)
@@ -50,12 +105,15 @@ public class FOXNamedEntityRecognition {
 
             //String url = "http://" + server.host + ":" + server.port;
 
-            String url = "http://fox-demo.aksw.org/api";
+            String url = "http://fox-demo.aksw.org";
             final Charset UTF_8 = Charset.forName("UTF-8");
             JSONObject jo = new JSONObject()
                     .put("type", "text")
                     .put("task", "NER")
-                    .put("output", "Turtle")
+                    .put("output", "JSON-LD")
+                    .put("disamb","off")
+                    .put("foxlight", FOX_LIGHT_STANFORD_EN)
+                    //.put("returnHtml", "false")
                     .put("input", s);
 
 
@@ -87,7 +145,6 @@ public class FOXNamedEntityRecognition {
         }catch (Exception e){
             return "";
         }
-
 
     }
 
